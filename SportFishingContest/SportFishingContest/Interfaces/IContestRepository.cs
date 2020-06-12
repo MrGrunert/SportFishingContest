b3878897dd5b9a06005c8a25692f0b5393a3a7da
Jimmy Grünert <jimmy.grunert@gmail.com>
2020-06-12 08:45:34 +0200

    added methods in ContestRepo

--- SportFishingContest/SportFishingContest/Interfaces/IContestRepository.cs
+++ SportFishingContest/SportFishingContest/Interfaces/IContestRepository.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using SportFishingContest.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace SportFishingContest.Interfaces
 {
     public interface IContestRepository
     {
+        Contest Add(Contest addContest);
+        Contest GetContestById(Guid id);
+        void UpdateContest(Contest update);
+        void DeleteContest(Contest delete);
+        IEnumerable<Contest> GetAllContests();
+        void Commit();
+
     }
 }
--- SportFishingContest/SportFishingContest/Repositories/ContestRepository.cs
+++ SportFishingContest/SportFishingContest/Repositories/ContestRepository.cs
@@ -1,4 +1,7 @@
-﻿using SportFishingContest.Interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using SportFishingContest.Data;
+using SportFishingContest.Interfaces;
+using SportFishingContest.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +11,53 @@ namespace SportFishingContest.Repositories
 {
     public class ContestRepository : IContestRepository
     {
+        private ApplicationDbContext _context;
+
+        public ContestRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Contest Add(Contest addContest)
+        {
+            _context.Add(addContest);
+            return addContest;
+        }
+
+        public void DeleteContest(Contest delete)
+        {
+            _context.Remove(delete);
+        }
+
+        public IEnumerable<Contest> GetAllContests()
+        {
+            return _context.Contests.ToList();
+        }
+
+        public Contest GetContestById(Guid id)
+        {
+            return _context.Contests.FirstOrDefault(c => c.Id == id);
+        }
+
+        public void UpdateContest(Contest update)
+        {
+            if (update == null)
+            {
+                var error = "An army of monkies broke the website...";
+                throw new ArgumentNullException(error);
+            }
+            if (update.Id == Guid.Empty)
+            {
+                throw new ArgumentException($"Catch id cannot be 0! {update.Id}");
+            }
+
+            _context.Contests.Update(update);
+            _context.SaveChanges();
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
     }
 }
